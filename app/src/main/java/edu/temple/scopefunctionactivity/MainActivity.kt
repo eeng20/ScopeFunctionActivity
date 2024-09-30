1f008ce8c57f3f99710cdf99642a9a6cefb05ec9
eeng20 <tul40997@temple.edu>
2024-09-30 16:04:22 -0400

    turnary for getView

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -30,9 +30,26 @@ class MainActivity : AppCompatActivity() {
     // HINT when constructing elaborate scope functions:
     // Look at the final/return value and build the function "working backwards"
 
+    // Return a list of random, sorted integers
+//    private fun getTestDataArray() : List<Int> {
+//        val testArray = MutableList(10){ Random.nextInt()}
+//        testArray.sort()
+//        return testArray
+//    }
 
     private fun getTestDataArray(): List<Int> = MutableList(10) { Random.nextInt() }.apply{sort()}
 
+    // Return true if average value in list is greater than median value, false otherwise
+//    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
+//        val avg = listOfNumbers.average()
+//        val sortedList = listOfNumbers.sorted()
+//        val median = if (sortedList.size % 2 == 0)
+//            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
+//        else
+//            sortedList[sortedList.size / 2]
+//
+//        return avg < median
+//    }
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>) =
@@ -43,8 +60,26 @@ class MainActivity : AppCompatActivity() {
             sortedList[sortedList.size / 2]
     }
 
+
+    // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
+//    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+//        val textView: TextView
+//
+//        if (recycledView != null) {
+//            textView = recycledView as TextView
+//        } else {
+//            textView = TextView(context)
+//            textView.setPadding(5, 10, 10, 0)
+//            textView.textSize = 22f
+//        }
+//
+//        textView.text = collection[position].toString()
+//
+//        return textView
+//    }
+
     private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View =
-        (recycledView as? TextView?: TextView(context)).apply {
-            setPadding(5, 10, 10, 0)
-            textSize = 22f }.apply { text = collection[position].toString() }
+        (recycledView as? TextView ?: TextView(context)).apply {
+            if (recycledView == null) {setPadding(5, 10, 10, 0); textSize = 22f }}.apply{
+                text = collection[position].toString() }
 }
